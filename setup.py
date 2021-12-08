b5b07f605ee53bbb706d64cb9d8337e96ada875d
reconsec <recon@securifera.com>
2021-12-08 20:12:19 +0000

    Created package

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+
+from setuptools import setup
+
+
+
+setup(name='waluigi',
+
+      version='1.0',
+
+      description='Scan automation pipeline',
+
+      author='Ryan Wincey (b0yd)',
+
+      author_email='rwincey@securifera.com',
+
+      url='https://www.securifera.com',
+
+      packages=['waluigi'],
+      install_requires=[
+
+          'luigi',
+          'pycryptodomex',
+          'pyshot'
+
+      ],
+
+     )

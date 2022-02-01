ab521a4ca911eece15a562840404f1a02b997d4b
root <root@ip-10-0-0-5.us-east-2.compute.internal>
2022-02-01 19:35:24 +0000

    Added reconnect loop

--- waluigi/scan_poller.py
+++ waluigi/scan_poller.py
@@ -2,6 +2,8 @@ import recon_manager
 import traceback
 import argparse
 import threading
+import time
+import sys
 
 local_extender_port = 33333
 
@@ -17,36 +19,39 @@ def main(args):
 
     # Create Synack connection manager thread
     scan_thread = None
+    while True:
+        try:
+            # Create instance of recon manager
+            recon_manager_inst =  recon_manager.get_recon_manager(args.token, "http://127.0.0.1:%d" % local_extender_port)
 
-    try:
-        # Create instance of recon manager
-        recon_manager_inst =  recon_manager.get_recon_manager(args.token, "http://127.0.0.1:%d" % local_extender_port)
+            # Create the scheduled scan thread
+            scan_thread = recon_manager.ScheduledScanThread(recon_manager_inst)
+            scan_thread.start()
 
-        # Create the scheduled scan thread
-        scan_thread = recon_manager.ScheduledScanThread(recon_manager_inst)
-        scan_thread.start()
-
-        # interactive console
-        while True:
-            print("Enter a command")
-            print(">", end="")
-            command = input()
-            if command == "q":
-                break
-            elif command == 'h':
-                print_usage()
-            elif command == 'x':
-                # Toggle the scan poller
-                scan_thread.toggle_poller()
-
-            
-    except Exception as e:
-        print(traceback.format_exc())
+            # interactive console
+            while True:
+                print("Enter a command")
+                print(">", end="")
+                command = input()
+                if command == "q":
+                    sys.exit(0)
+                    break
+                elif command == 'h':
+                    print_usage()
+                elif command == 'x':
+                    # Toggle the scan poller
+                    scan_thread.toggle_poller()
 
 
-    # Stop scan scheduler thread
-    if scan_thread:
-        scan_thread.stop()
+        except Exception as e: 
+            if "refused" in str(e):
+                print("[*] Connection done. Retrying in 30 seconds")
+                time.sleep(30) # Stop scan scheduler thread
+                continue
+            break
+
+        if scan_thread:
+            scan_thread.stop()
 
 
 if __name__ == "__main__":
